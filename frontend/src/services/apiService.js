bcc8adadca3f70ce022bca6773e16ff5384aab77
Swapnamoy Bhowmick <swapnamoy31@gmail.com>
2024-05-02 02:53:37 -0400

    relogin after token expires

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,3 +1,5 @@
+const TOKEN_EXPIRED = "The incoming token has expired"
+
 const apiCall = async ({ method, url, data = null, headers = {}, params = {} }, apiClient) => {
   try {
     const config = {
@@ -11,6 +13,9 @@ const apiCall = async ({ method, url, data = null, headers = {}, params = {} }, 
     const response = await apiClient.request(config);
     return response.data;
   } catch (error) {
+    if (error.response && error.response.data.message === TOKEN_EXPIRED) {
+      window.location.href = '/login';
+    }
     console.error(`Error with ${method} request to ${url}:`, error);
     throw error;
   }
