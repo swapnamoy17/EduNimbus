d4694ba530ec351241a78bd5434d8dd0615354c2
Swarali Dabhadkar <57556470+swavida@users.noreply.github.com>
2024-05-02 22:03:10 -0400

    modified:   frontend/src/services/apiClient.js

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -18,4 +18,22 @@ videoApiClient.interceptors.request.use((config) => {
     return Promise.reject(error);
 });
 
+const courseApiClient = axios.create({
+  baseURL: process.env.REACT_APP_COURSE_GATEWAY_ENDPOINT,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+courseApiClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    console.log("courseAPIClient - token: ", token);
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
 export { videoApiClient }
+export { courseApiClient }
